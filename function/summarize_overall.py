93ba6149ba729e975afff9135fda1d6729fca956
laeag <laeag@naver.com>
2024-10-04 13:36:23 +0900

    Update summarize_overall.py

--- function/summarize_overall.py
+++ function/summarize_overall.py
@@ -9,10 +9,12 @@ def summarize_overall(text):
     inputs = tokenizer([text], max_length=1024, return_tensors='pt', truncation=True)
     summary_ids = model.generate(
         inputs['input_ids'], 
-        num_beams=4, 
+        num_beams=8, 
         max_length=256, 
         min_length=100, 
-        no_repeat_ngram_size=2, 
+        temperature=0.7,  # 다양성 조절
+        top_p=0.9,
+        no_repeat_ngram_size=3, # 반복 방지
         early_stopping=True)
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return summary
