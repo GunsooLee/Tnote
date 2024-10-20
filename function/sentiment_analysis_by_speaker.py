2f9ddc43fd0269b1a70f7126f4b187fb071e4498
laeag <laeag@naver.com>
2024-10-21 08:38:39 +0900

    Update sentiment_analysis_by_speaker.py

--- function/sentiment_analysis_by_speaker.py
+++ function/sentiment_analysis_by_speaker.py
@@ -1,15 +1,19 @@
 from transformers import pipeline
 
 # 감정 분석 모델 로드
-emotion_model = pipeline('sentiment-analysis')
+sentiment_pipeline = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
 
 # 화자별 감정 분석 함수
 def analyze_emotion_by_speaker(df):
     speaker_groups = df.groupby('화자')['분석된 내용'].apply(' '.join)  # 화자별로 내용 결합
     speaker_emotions = {}
+    labels = []
+    scores = []
 
     for speaker, combined_text in speaker_groups.items():
-        emotions = emotion_model(combined_text)  # 감정 분석
-        speaker_emotions[speaker] = emotions
+        emotions = sentiment_pipeline(combined_text)[0]  # 감정 분석
+        labels.append(f"화자 {speaker}: {emotions['label']}")
+        scores.append(emotions['score'])
+        #speaker_emotions[speaker] = emotions
 
-    return speaker_emotions
+    return labels, scores
