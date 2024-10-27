3e71686cdb2751ad25d079074d4f30e9a5e4a22f
laeag <laeag@naver.com>
2024-10-28 08:46:42 +0900

    화자별 분석 내용 앞뒤로 큰따옴표 추가, min_length 줄임

--- function/summarize_by_speaker.py
+++ function/summarize_by_speaker.py
@@ -6,12 +6,12 @@ model = BartForConditionalGeneration.from_pretrained('./kobart-finetuned')
 
 # 화자별 요약 함수
 def summarize_by_speaker(df):
-    speaker_groups = df.groupby('화자')['내용'].apply(' '.join)  # 화자별로 내용 결합
+    speaker_groups = df.groupby('화자')['내용'].apply(lambda x: f"{x.name}: \"{' '.join(x)}\".")  # 화자별로 내용 결합
     speaker_summaries = {}
 
     for speaker, combined_text in speaker_groups.items():
         inputs = tokenizer([combined_text], max_length=1024, return_tensors='pt', truncation=True)
-        summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=256, min_length=100, early_stopping=True)
+        summary_ids = model.generate(inputs['input_ids'], num_beams=4, max_length=256, min_length=16, early_stopping=True)
         summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
         speaker_summaries[speaker] = summary
 
